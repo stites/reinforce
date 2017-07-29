56d7f48e73baf3440249a15a0cd5befefe27ef09
stites <sam@stites.io>
2017-07-29 18:50:51 -0400

    add hlint suggestions

--- src/REINFORCE.hs
+++ src/REINFORCE.hs
@@ -13,15 +13,17 @@ import qualified TensorFlow.GenOps.Core  as TF (square)
 import qualified Data.Vector as V
 
 
-max_episodes, max_steps, batch_size, hidden_size :: Int32
-lr, gamma :: Float
+maxEpisodes, maxSteps, batchSize, hiddenSize :: Int32
+gamma :: Float
 
-max_episodes = 2000
-max_steps    = 99
-batch_size   = 5
-hidden_size  = 8
-lr           = 0.1
+maxEpisodes = 2000
+maxSteps    = 99
+batchSize   = 5
+hiddenSize  = 8
 gamma        = 0.99
+
+lr :: Tensor Build Float
+lr = TF.scalar 0.1
 
 -- ========================================================================= --
 -- Build Tensorflow graph
@@ -44,17 +46,17 @@ data GraphRefs = GraphRefs
 createAgent :: (Enum a, StateSpace s) => Build (Model s a)
 createAgent = mkAgentGraph 1 16 32
   >>= \vars -> Model
-    <$> (runReaderT infer vars)
-    <*> (runReaderT train vars)
+    <$> runReaderT infer vars
+    <*> runReaderT train vars
 
 
 mkAgentGraph :: Int64 -> Int64 -> Int64 -> TF.Build GraphRefs
-mkAgentGraph action_size state_size hidden_size = do
-  network <- mkFullyConnected action_size state_size hidden_size
+mkAgentGraph actionSize stateSize hiddenSize = do
+  network <- mkFullyConnected actionSize stateSize hiddenSize
   let qOut   = getOutputs network
       inputs = getInputs network
   predict <- TF.render . TF.cast $ TF.argMax qOut (TF.scalar (1 :: TFAction))
-  nextQs  <- TF.placeholder [ 1, action_size]
+  nextQs  <- TF.placeholder [ 1, actionSize]
   return GraphRefs
     { network = network
     , predict = predict
@@ -68,8 +70,8 @@ infer = do
   let
     inputs = getInputs network
     qOut   = getOutputs network
-    (_, state_size, _)  = shape network
-    encodeState = encodeState' state_size
+    (_, stateSize, _)  = shape network
+    encodeState = encodeState' stateSize
   return $ \st -> do
     a  <- decodeAction <$> TF.runWithFeeds [TF.feed inputs (encodeState st)] predict
     qs <-                  TF.runWithFeeds [TF.feed inputs (encodeState st)] qOut
@@ -80,18 +82,22 @@ train :: ReaderT GraphRefs Build (TensorData Float -> TensorData Float -> Sessio
 train = do
   GraphRefs{network, nextQs} <- ask
   let
+    inputs :: Tensor Value Float
     inputs = getInputs network
+
+    qOut :: Tensor Build Float
     qOut = getOutputs network
-    loss :: Tensor Build Float
-    loss = TF.reduceSum $ TF.square (nextQs `TF.sub` qOut)
 
     weights :: [Tensor Ref Float]
     weights = getWeights network
 
+    loss :: Tensor Build Float
+    loss = TF.reduceSum $ TF.square (nextQs `TF.sub` qOut)
+
   grads     <- lift $ TF.gradients     loss weights
   trainStep <- lift $ applyGradients weights  grads
 
-  return $ \inFeed rwdFeed -> do
+  return $ \inFeed rwdFeed ->
     TF.runWithFeeds_
       [ TF.feed inputs inFeed
       , TF.feed nextQs rwdFeed
@@ -104,8 +110,6 @@ train = do
         applyGrad :: Tensor Ref Float -> Tensor Value Float -> Build (Tensor Ref Float)
         applyGrad param grad = TF.assign param $ param `TF.sub` (lr `TF.mul` grad)
 
-        lr :: Tensor Build Float
-        lr = TF.scalar 0.00001
 
 
 
