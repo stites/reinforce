35eb34bbb9fb2050d46bf7eaecc6f6a0b19e1358
stites <sam@stites.io>
2018-07-18 16:39:49 -0400

    add blackjack example

--- reinforce-zoo/blackjack/Main.hs
+++ reinforce-zoo/blackjack/Main.hs
@@ -1,3 +1,16 @@
 module Main where
 
-main = undefined
+import Reinforce.Agents
+import Reinforce.Agents.QTable
+import Reinforce.Algorithms.QLearning
+
+import Environments.Gym.ToyText.BlackjackV0 (runDefaultEnvironment)
+
+main :: IO ()
+main = do
+  x <- runDefaultEnvironment False $
+         runQTable defaultConfigs (Left 0.85) $
+           runLearner (Just 10) (Just 10)
+             rolloutQLearning
+  print x
+
